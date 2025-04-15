a7abc849ebda061646ab9303077b826a66fde45d
Hashir Shahzad <hashir447079@gmail.com>
2025-04-15 11:33:52 +0500

    ADded ads

---
+++ ad_window/ad_resource.gd
@@ -0,0 +1,6 @@
+extends Resource
+class_name AdResource
+
+@export var url : String = "https://duckduckgo.com/"
+@export var img : CompressedTexture2D
+@export var label_text : String = "Get free cat"
--- ad_window/ad_window.gd
+++ ad_window/ad_window.gd
@@ -1,2 +1,20 @@
 extends Window
 class_name AdWindows
+
+@export var ad_array : Array[AdResource]
+var ad_res : AdResource
+
+@onready var label: Label = $PanelContainer/CenterContainer/Label
+@onready var texture_rect : TextureRect = $PanelContainer/TextureRect
+
+func _ready() -> void:
+	_on_create()
+
+func _on_create() -> void:
+	ad_res = ad_array.pick_random()
+	texture_rect.texture = ad_res.img
+	label.text = ad_res.label_text
+	
+
+func _on_get_button_pressed() -> void:
+	OS.shell_open(ad_res.url)
