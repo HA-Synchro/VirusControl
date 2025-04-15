6c464e26074255656a8e0cd4514556a7b660cf79
Hashir Shahzad <hashir447079@gmail.com>
2025-04-15 11:43:47 +0500

    made it a lil better :Z

--- ad_window/ad_window.gd
+++ ad_window/ad_window.gd
@@ -4,13 +4,13 @@ class_name AdWindows
 @export var ad_array : Array[AdResource]
 var ad_res : AdResource
 var desktop_env : DesktopEnvironment 
-@onready var label: Label = $PanelContainer/CenterContainer/Label
+
 @onready var texture_rect : TextureRect = $PanelContainer/TextureRect
 
 func _ready() -> void:
 	ad_res = ad_array.pick_random()
 	texture_rect.texture = ad_res.img
-	label.text = ad_res.label_text
+	self.title = ad_res.label_text
 
 func _on_get_button_pressed() -> void:
 	OS.shell_open(ad_res.url)
